9156ec9f5c2c57379d84935ee81c52e7eac3b2cc
calvinadhikang <calvinadhikang@gmail.com>
2023-12-16 17:55:21 +0700

    fix menu module

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MenuEntity } from 'src/typeorm/entities/menu.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([MenuModule])],
+    imports: [TypeOrmModule.forFeature([MenuEntity])],
     controllers: [MenuController],
     providers: [MenuService]
 })
--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -5,12 +5,4 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class MenuService {
-    constructor(
-        @InjectRepository(MenuEntity)
-        private menuRepository: Repository<MenuEntity>
-    ) {}
-
-    getMenuByResto(id: number){
-        return this.menuRepository.find
-    }
 }
